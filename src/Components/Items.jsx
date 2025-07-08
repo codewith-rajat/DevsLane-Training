b4045bc9c1c18cafcff45b88de8fbfcc073a26e3
codewith-rajat <rajatagrawal021@gmail.com>
2025-07-08 04:08:16 +0000

    changes in 54.1

--- src/Components/Items.jsx
+++ src/Components/Items.jsx
@@ -5,7 +5,7 @@ export default function Items({image, name, category,price}) {
     <div className='bg-white pt-8 w-80'>
       <img src={image} alt={name} className="w-full h-70 object-fill" />
         <p className="text-gray-600 mt-2">{category}</p>
-        <h2 className="text-xl font-bold mt-2">{name}</h2>
+        <h2 className="text-xl font-bold mt-1">{name}</h2>
         <p className="text-lg font-semibold mt-2">${price}</p>
     </div>
   )
